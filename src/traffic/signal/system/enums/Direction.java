e803883fe0979a078a8bb2bfa0a94e4bd9ba77fd
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-14 08:36:45 +0530

    Case Study: 05 Traffic Control System | Light Color and Direction Enums

---
+++ src/traffic/signal/system/enums/Direction.java
@@ -0,0 +1,8 @@
+package traffic.signal.system.enums;
+
+public enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
---
+++ src/traffic/signal/system/enums/Light.java
@@ -0,0 +1,7 @@
+package traffic.signal.system.enums;
+
+public enum Light {
+    GREEN,
+    YELLOW,
+    RED
+}

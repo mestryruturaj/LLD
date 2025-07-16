36a04286ea3d215e74845d3ab5a075831ef021b4
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-16 22:33:16 +0530

    Case Study 06: Coffee Vending Machine | Demo

--- src/cases/coffee/vending/machine/Client.java
+++ src/cases/coffee/vending/machine/Client.java
@@ -1,4 +1,60 @@
 package cases.coffee.vending.machine;
 
+import cases.coffee.vending.machine.model.Coin;
+import cases.coffee.vending.machine.model.Ingredients;
+import cases.coffee.vending.machine.model.ItemDetail;
+import cases.coffee.vending.machine.model.StateType;
+import cases.coffee.vending.machine.recipe.CappuccinoRecipe;
+import cases.coffee.vending.machine.recipe.EspressoRecipe;
+import cases.coffee.vending.machine.recipe.LatteRecipe;
+import cases.coffee.vending.machine.recipe.RegularCoffeeRecipe;
+import cases.coffee.vending.machine.states.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Client {
+    public static void main(String[] args) {
+        //item maker
+        ItemMaker itemMaker = new ItemMaker();
+
+        //item details
+        ItemDetail regularCoffeeDetail = new ItemDetail("Regular Coffee", 6, Map.of(Ingredients.COFFEE_BEANS, 10, Ingredients.MILK, 5), new RegularCoffeeRecipe());
+        ItemDetail cappuccinoDetail = new ItemDetail("Cappuccino", 5, Map.of(Ingredients.COFFEE_BEANS, 8, Ingredients.MILK, 7), new CappuccinoRecipe());
+        ItemDetail LatteDetail = new ItemDetail("Latte", 4, Map.of(Ingredients.COFFEE_BEANS, 11, Ingredients.MILK, 2), new LatteRecipe());
+        ItemDetail espressoDetail = new ItemDetail("Espresso", 8, Map.of(Ingredients.COFFEE_BEANS, 11, Ingredients.MILK, 3), new EspressoRecipe());
+        Map<String, ItemDetail> items = new HashMap<>();
+        items.put(regularCoffeeDetail.getName(), regularCoffeeDetail);
+        items.put(cappuccinoDetail.getName(), cappuccinoDetail);
+        items.put(LatteDetail.getName(), LatteDetail);
+        items.put(espressoDetail.getName(), espressoDetail);
+
+        //possible states
+        State readyState = new ReadyState(StateType.READY);
+        State itemSelectedState = new ItemsSelectedState(StateType.ITEMS_SELECTED);
+        State paidState = new PaidState(StateType.PAID);
+        State dispensedState = new DispensedState(StateType.DISPENSED);
+        Map<StateType, State> possibleState = new HashMap<>();
+        possibleState.put(StateType.READY, readyState);
+        possibleState.put(StateType.ITEMS_SELECTED, itemSelectedState);
+        possibleState.put(StateType.PAID, paidState);
+        possibleState.put(StateType.DISPENSED, dispensedState);
+
+        CoffeeVendingMachine coffeeVendingMachine = new CoffeeVendingMachine(items, itemMaker, possibleState);
+
+        coffeeVendingMachine.displayItems();
+        System.out.println();
+
+        coffeeVendingMachine.selectItems(new HashMap<>(Map.of(regularCoffeeDetail, 1, LatteDetail, 1)));
+        System.out.println();
+        coffeeVendingMachine.makePayment(new ArrayList<>(List.of(Coin.FIVE, Coin.FIVE, Coin.TWO, Coin.ONE, Coin.FIVE)));
+        System.out.println();
+        coffeeVendingMachine.dispense();
+        System.out.println();
+        coffeeVendingMachine.reset();
+        System.out.println();
+
+    }
 }
--- src/cases/vending/machine/Client.java
+++ src/cases/vending/machine/Client.java
@@ -4,31 +4,13 @@ import cases.vending.machine.enums.Coin;
 import cases.vending.machine.enums.ItemType;
 import cases.vending.machine.factory.StateRegistry;
 import cases.vending.machine.model.*;
-import vending.machine.model.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Client {
     public static void main(String[] args) {
-        Item item1 = new Item(ItemType.COKE);
-        Item item2 = new Item(ItemType.PROTEIN_BAR);
-        Item item3 = new Item(ItemType.MOUNTAIN_DEW);
-
-        Rack rack1 = new Rack(101, ItemType.COKE, 3);
-        Rack rack2 = new Rack(102, ItemType.PROTEIN_BAR, 7);
-        Rack rack3 = new Rack(103, ItemType.MOUNTAIN_DEW, 1);
-        rack1.addItem(item1);
-        rack2.addItem(item2);
-        rack3.addItem(item3);
-
-        Shelf shelf = new Shelf();
-        shelf.addRack(rack1);
-        shelf.addRack(rack2);
-        shelf.addRack(rack3);
-
-        Inventory inventory = new Inventory();
-        inventory.addShelf(shelf);
+        Inventory inventory = getInventory();
 
         Map<ItemType, Integer> prices = new HashMap<>();
         prices.put(ItemType.COKE, 3);
@@ -47,6 +29,28 @@ public class Client {
         vendingMachine.makePayment();
 
         vendingMachine.dispense();
+    }
+
+    private static Inventory getInventory() {
+        Item item1 = new Item(ItemType.COKE);
+        Item item2 = new Item(ItemType.PROTEIN_BAR);
+        Item item3 = new Item(ItemType.MOUNTAIN_DEW);
+
+        Rack rack1 = new Rack(101, ItemType.COKE, 3);
+        Rack rack2 = new Rack(102, ItemType.PROTEIN_BAR, 7);
+        Rack rack3 = new Rack(103, ItemType.MOUNTAIN_DEW, 1);
+        rack1.addItem(item1);
+        rack2.addItem(item2);
+        rack3.addItem(item3);
+
+        Shelf shelf = new Shelf();
+        shelf.addRack(rack1);
+        shelf.addRack(rack2);
+        shelf.addRack(rack3);
+
+        Inventory inventory = new Inventory();
+        inventory.addShelf(shelf);
+        return inventory;
     }
 }
 

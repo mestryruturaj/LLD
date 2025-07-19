34fac92aa57a9abab6b0a4dd12b751290102d7e6
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-19 13:12:25 +0530

    Case Study 07: Task Management System | Task Log

--- src/cases/task/management/system/models/TaskLog.java
+++ src/cases/task/management/system/models/TaskLog.java
@@ -11,4 +11,58 @@ public class TaskLog {
     private TaskState taskState;
     private User user;
     private Timestamp timestamp;
+
+    public TaskLog(Task task, TaskState taskState, User user, Timestamp timestamp) {
+        this.id = counter++;
+        this.task = task;
+        this.taskState = taskState;
+        this.user = user;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public TaskState getTaskState() {
+        return taskState;
+    }
+
+    public void setTaskState(TaskState taskState) {
+        this.taskState = taskState;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskLog{" +
+                "\nid=" + id +
+                "\ntask=" + task +
+                "\ntaskState=" + taskState +
+                "\ntimestamp=" + timestamp +
+                "\n}";
+    }
 }

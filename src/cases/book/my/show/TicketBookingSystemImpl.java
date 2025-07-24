163b012e17b8ed15892736242a86e262718fa0d1
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-07-24 08:26:24 +0530

    Case Study 09: TicketBookingSystem | initial contract of the system

---
+++ src/cases/book/my/show/TicketBookingSystemImpl.java
@@ -0,0 +1,61 @@
+package cases.book.my.show;
+
+import cases.book.my.show.exception.TicketBookingSystemException;
+import cases.book.my.show.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static cases.book.my.show.model.enums.SeatStatus.BOOKED;
+
+public class TicketBookingSystemImpl implements TicketBookingSystem {
+    private Map<Show, List<ShowSeat>> bookedShowSeats;
+
+    @Override
+    public synchronized Ticket bookTicket(Show show, Screen screen, List<int[]> seats, User user) {
+        if (!checkWhetherSeatsAvailable(show, seats)) {
+            throw new TicketBookingSystemException("Selected seats are unavailable.");
+        }
+        List<ShowSeat> bookedSeats = new ArrayList<>();
+        bookedShowSeats.putIfAbsent(show, new ArrayList<>());
+        for (int[] cell : seats) {
+            for (Seat seat : screen.getSeats()) {
+                if (seat.getRow() == cell[0] && seat.getCol() == cell[1]) {
+                    ShowSeat curShowSeat = new ShowSeat(show, seat);
+                    curShowSeat.setSeatStatus(BOOKED);
+                    bookedSeats.add(curShowSeat);
+                    break;
+                }
+            }
+        }
+
+        //make payment
+
+        //
+        bookedShowSeats.get(show).addAll(bookedSeats);
+        Ticket ticket = new Ticket(bookedSeats, user);
+        return ticket;
+    }
+
+    private boolean checkWhetherSeatsAvailable(Show show, List<int[]> seats) {
+        if (!bookedShowSeats.containsKey(show)) {
+            return true;
+        }
+
+        for (int[] cell: seats) {
+            for (ShowSeat showSeat : bookedShowSeats.get(show)) {
+                if (cell[0] == showSeat.getSeat().getRow() && cell[1] == showSeat.getSeat().getCol()) {
+                    return false;
+                }
+             }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<Ticket> getAllTicketsForUser(User user) {
+        return user.getBookedTickets();
+    }
+}
